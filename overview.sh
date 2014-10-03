e307c8da2201567ca18ceaef0a85031b06163e04
Thiago Perrotta <thiagoperrotta95@gmail.com>
2014-10-03 17:13:34 -0300

    improve graph generation tool

--- overview.sh
+++ overview.sh
@@ -3,5 +3,5 @@
 
 [[ $1 == "" ]] && echo "Usage: $0 [-g|-l]" && exit
 
-[[ $1 == "-g" ]] && ./ext/cinclude2dot.pl
+[[ $1 == "-g" ]] && ./ext/cinclude2dot.pl > build/graph.dot && neato -Tsvg build/graph.dot > build/graph.svg && xdg-open build/graph.svg
 [[ $1 == "-l" ]] && tree ext include src test
