e11a854fd2dd69463ffeff2d5c6d7f28544f1491
Thiago Perrotta <thiagoperrotta95@gmail.com>
2014-10-03 15:57:25 -0300

    add format.sh

---
+++ format.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Try to detect clang on the system.
+[[ -x $(which clang-format) ]] && CLANG_FORMAT=clang-format
+[[ -x $(which clang-format-3.5) ]] && CLANG_FORMAT=clang-format-3.5
+[[ -z $CLANG_FORMAT ]] && echo "> clang-format not found on this system." && exit
+
+$CLANG_FORMAT --style=Google -i include/*.hpp src/*.cpp test/*.cpp && echo "> Files succesfully formatted."
