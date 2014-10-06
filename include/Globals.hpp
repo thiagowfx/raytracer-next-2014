3121aa5550543f0603fcca40f44196af9b6d226c
Thiago Perrotta <thiagoperrotta95@gmail.com>
2014-10-06 19:44:18 -0300

    Globals added (tests, ..)

--- include/Globals.hpp
+++ include/Globals.hpp
@@ -3,15 +3,15 @@
 
 #include <cstdlib>
 #include <ctime>
-#include <fstream>
 #include <iostream>
+#include <fstream>
 #include <boost/serialization/nvp.hpp>
 #include <boost/serialization/split_free.hpp>
 #include <boost/serialization/vector.hpp>
-#include <boost/archive/binary_iarchive.hpp>
 #include <boost/archive/binary_oarchive.hpp>
-#include <boost/archive/xml_iarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
 #include <boost/archive/xml_oarchive.hpp>
+#include <boost/archive/xml_iarchive.hpp>
 using boost::serialization::make_nvp;
 #include <Eigen/Dense>
 
@@ -31,6 +31,7 @@ inline void serialize(
 }
 }
 
+/** Serialize a class into the XML format. */
 template <class T>
 void save_xml(const T& t, const char* filename) {
   std::ofstream ofs(filename);
@@ -38,6 +39,7 @@ void save_xml(const T& t, const char* filename) {
   oa << BOOST_SERIALIZATION_NVP(t);
 }
 
+/** Load a serialized class from the XML format. */
 template <class T>
 void load_xml(T& t, const char* filename) {
   std::ifstream ifs(filename);
@@ -45,6 +47,7 @@ void load_xml(T& t, const char* filename) {
   ia >> BOOST_SERIALIZATION_NVP(t);
 }
 
+/** Serialize a class into a binary format. */
 template <class T>
 void save_binary(const T& t, const char* filename) {
   std::ofstream ofs(filename);
@@ -52,11 +55,87 @@ void save_binary(const T& t, const char* filename) {
   oa << BOOST_SERIALIZATION_NVP(t);
 }
 
+/** Load a serialized class from a binary format. */
 template <class T>
 void load_binary(T& t, const char* filename) {
   std::ifstream ifs(filename);
   boost::archive::binary_iarchive ia(ifs);
   ia >> BOOST_SERIALIZATION_NVP(t);
 }
+
+/** Small constant for shadows. */
+const double kEpsilonShadows = 1.0e-3;
+
+/** Small constant. */
+const double kEpsilon = 1.0e-6;
+
+/** Huge constant. */
+const double kHugeValue = 1.0e10;
+
+/** Ambient Constant. Should be from 0.0 to 1.0. */
+const double kKa = 0.15;
+
+/** Diffuse Constant. Should be from 0.0 to 1.0. */
+const double kKd = 0.70;
+
+/** Specular Constant. Should be from 0.0 to 1.0. */
+const double kKs = 0.15;
+
+/** Specular Exponent from cosine. A reasonable number is from 1.0 to 20.0. */
+const double kExp = 12.0;
+
+/** Reflective Constant. */
+const double kKr = 0.75;
+
+/** Return a random integer, from begin to end, or from 0 to RAND_MAX. */
+int get_random_int(int begin = 0, int end = RAND_MAX);
+
+/** Return a random double, from begin to end, or from 0.0 to 1.0. */
+double get_random_double(double begin = 0.0, double end = 1.0);
+
+/** Return true if the argument is sufficiently near zero. */
+bool is_zero(const double);
+
+/** Generate a new seed for randomness. */
+void generate_random_seed();
+
+/**
+ *  Utility function to find quadric roots. Coefficients are passed
+ *  like this:
+ *
+ *  c[0] + c[1]*x + c[2]*x^2 = 0
+ *
+ *  The function return the number of non-complex roots and
+ *  put the values into the s array.
+ *
+ *  Author: Jochen Schwarze <schwarze@isa.de>
+ */
+int solve_quadric(double c[3], double s[2]);
+
+/**
+ *  Utility function to find cubic roots. Coefficients are passed
+ *  like this:
+ *
+ *  c[0] + c[1]*x + c[2]*x^2 + c[3]*x^3 = 0
+ *
+ *  The function return the number of non-complex roots and
+ *  put the values into the s array.
+ *
+ *  Author: Jochen Schwarze <schwarze@isa.de>
+ */
+int solve_cubic(double c[4], double s[3]);
+
+/**
+ *  Utility function to find quartic roots. Coefficients are passed
+ *  like this:
+ *
+ *  c[0] + c[1]*x + c[2]*x^2 + c[3]*x^3 + c[4]*x^4 = 0
+ *
+ *  The function return the number of non-complex roots and
+ *  put the values into the s array.
+ *
+ *  Author: Jochen Schwarze <schwarze@isa.de>
+ */
+int solve_quartic(double c[5], double s[4]);
 
 #endif  // _GLOBALS_H_
